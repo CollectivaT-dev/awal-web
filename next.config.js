0d75039572e6514edc8dab3bda20d25f1c96ecc9
ppalladio <nine.slide@gmail.com>
2024-05-27 20:52:26 +0200

    add md package, waiting for config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const withMDX = require('@next/mdx')();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
     typescript: {
         ignoreBuildErrors: true,
     },
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig)
